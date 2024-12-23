f4e037aabd17efa8ebdca943e26bb970d73b8fe2
Mirailisc <mirailisclm@gmail.com>
2024-12-23 20:59:13 +0700

    improve age consent

--- frontend/src/components/Utils/AgeConsent.tsx
+++ frontend/src/components/Utils/AgeConsent.tsx
@@ -4,8 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { AGE_CONSENT } from '@constants/local-storage'
 
 const AgeConsent: React.FC = (): JSX.Element => {
-  const [isVisible, setIsVisible] = useState<boolean>(true)
-
+  const [isVisible, setIsVisible] = useState<boolean>(false)
   const navigate = useNavigate()
 
   const onAccept = () => {
@@ -15,62 +14,60 @@ const AgeConsent: React.FC = (): JSX.Element => {
 
   const onDecline = () => {
     localStorage.setItem(AGE_CONSENT, 'false')
-    setIsVisible(true)
+    setIsVisible(false)
     navigate(-1)
   }
 
   useEffect(() => {
     const ageConsent = localStorage.getItem(AGE_CONSENT)
 
-    if (ageConsent) {
-      setIsVisible(ageConsent === 'false')
-    } else {
+    if (ageConsent === null) {
       localStorage.setItem(AGE_CONSENT, 'false')
+      setIsVisible(true)
+    } else if (ageConsent === 'false') {
+      setIsVisible(true)
+    } else {
       setIsVisible(false)
     }
 
-    if (isVisible) {
-      document.body.style.overflow = 'hidden'
-    } else {
-      document.body.style.overflow = 'auto'
-    }
+    document.body.style.overflow = isVisible ? 'hidden' : 'auto'
 
     return () => {
       document.body.style.overflow = 'auto'
     }
-  }, [isVisible])
+  }, [])
 
-  if (isVisible) {
-    return (
-      <div className="absolute inset-0 z-50 flex min-h-screen items-center justify-center overflow-hidden bg-black/80 px-4 sm:px-0">
-        <div className="w-full rounded-lg border border-white/20 bg-background p-4 text-white sm:w-[500px]">
-          <h1 className="text-center text-xl font-bold">Age Verification</h1>
-          <div className="text-md my-4 text-center">
-            This website contains explicit adult content that is intended for users over the age of 18 (or the age of
-            majority in your jurisdiction). By accessing this site, you confirm that you are of legal age to view such
-            content. If you are under the legal age or if adult content is prohibited in your area, you must leave this
-            website immediately.
-          </div>
-          <div className="flex justify-center gap-4">
-            <button
-              onClick={onAccept}
-              className="rounded bg-primary-700 px-4 py-2 text-white transition hover:bg-primary-800"
-            >
-              I am over 18
-            </button>
-            <button
-              onClick={onDecline}
-              className="rounded bg-secondary-700 px-4 py-2 text-white transition hover:bg-secondary-800"
-            >
-              I am under 18
-            </button>
-          </div>
-        </div>
-      </div>
-    )
-  } else {
+  if (!isVisible) {
     return <></>
   }
+
+  return (
+    <div className="absolute inset-0 z-50 flex min-h-screen items-center justify-center overflow-hidden bg-black/80 px-4 sm:px-0">
+      <div className="w-full rounded-lg border border-white/20 bg-background p-4 text-white sm:w-[500px]">
+        <h1 className="text-center text-xl font-bold">Age Verification</h1>
+        <div className="text-md my-4 text-center">
+          This website contains explicit adult content that is intended for users over the age of 18 (or the age of
+          majority in your jurisdiction). By accessing this site, you confirm that you are of legal age to view such
+          content. If you are under the legal age or if adult content is prohibited in your area, you must leave this
+          website immediately.
+        </div>
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={onAccept}
+            className="rounded bg-primary-700 px-4 py-2 text-white transition hover:bg-primary-800"
+          >
+            I am over 18
+          </button>
+          <button
+            onClick={onDecline}
+            className="rounded bg-secondary-700 px-4 py-2 text-white transition hover:bg-secondary-800"
+          >
+            I am under 18
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AgeConsent
