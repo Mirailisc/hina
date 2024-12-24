e6e9fff2835a025fc032e65f68daace86e735288
Mirailisc <mirailisclm@gmail.com>
2024-12-25 00:24:38 +0700

    fix

--- backend/src/search/search.resolver.ts
+++ backend/src/search/search.resolver.ts
@@ -2,7 +2,6 @@ import { Resolver, Query, Args } from '@nestjs/graphql'
 import { SearchService } from './search.service'
 import { MangaSearch } from './entities/MangaSearch.entity'
 import { SearchInput } from './dto/search.input'
-import { SearchTagsInput } from './dto/search-tags.input'
 
 @Resolver(() => MangaSearch)
 export class SearchResolver {
@@ -13,17 +12,5 @@ export class SearchResolver {
     @Args('input') input: SearchInput,
   ): Promise<MangaSearch[]> {
     return await this.searchService.search(input.name, input.amount, input.page)
-  }
-
-  @Query(() => [MangaSearch])
-  async searchByTags(
-    @Args('input') input: SearchTagsInput,
-  ): Promise<MangaSearch[]> {
-    return await this.searchService.searchByTag(
-      input.includedTags,
-      input.excludedTags,
-      input.amount,
-      input.page,
-    )
   }
 }
