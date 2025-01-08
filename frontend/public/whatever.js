75702491c1b8e53e3defb73ca39490e2cc7a0b6e
Mirailisc <mirailisclm@gmail.com>
2025-01-08 21:13:40 +0700

    feat: prevent user from using devtools

---
+++ frontend/public/whatever.js
@@ -0,0 +1,89 @@
+function _0x1491(_0x374fe3, _0x12f206) {
+  var _0x42d843 = _0x42d8()
+  return (
+    (_0x1491 = function (_0x1491f0, _0x5b4f9d) {
+      _0x1491f0 = _0x1491f0 - 0x15c
+      var _0x587baf = _0x42d843[_0x1491f0]
+      return _0x587baf
+    }),
+    _0x1491(_0x374fe3, _0x12f206)
+  )
+}
+;(function (_0x1172c8, _0x3a7418) {
+  var _0x150c94 = _0x1491,
+    _0x269466 = _0x1172c8()
+  while (!![]) {
+    try {
+      var _0x1ab3f8 =
+        (-parseInt(_0x150c94(0x16a)) / 0x1) * (parseInt(_0x150c94(0x174)) / 0x2) +
+        (-parseInt(_0x150c94(0x170)) / 0x3) * (-parseInt(_0x150c94(0x168)) / 0x4) +
+        (-parseInt(_0x150c94(0x171)) / 0x5) * (-parseInt(_0x150c94(0x167)) / 0x6) +
+        (parseInt(_0x150c94(0x165)) / 0x7) * (parseInt(_0x150c94(0x16e)) / 0x8) +
+        (-parseInt(_0x150c94(0x163)) / 0x9) * (-parseInt(_0x150c94(0x16d)) / 0xa) +
+        (-parseInt(_0x150c94(0x166)) / 0xb) * (-parseInt(_0x150c94(0x15c)) / 0xc) +
+        -parseInt(_0x150c94(0x15e)) / 0xd
+      if (_0x1ab3f8 === _0x3a7418) break
+      else _0x269466['push'](_0x269466['shift']())
+    } catch (_0x470039) {
+      _0x269466['push'](_0x269466['shift']())
+    }
+  }
+})(_0x42d8, 0xa99a1),
+  !(function () {
+    var _0x2994ca = _0x1491
+    function _0x3c224c(_0x18438d) {
+      var _0x4da5e2 = _0x1491
+      if (isNaN(+_0x18438d)) _0x18438d = 0x64
+      var _0x56b7ea = +new Date()
+      debugger
+      var _0x5e9b9e = +new Date()
+      ;(isNaN(_0x56b7ea) || isNaN(_0x5e9b9e) || _0x5e9b9e - _0x56b7ea > _0x18438d) &&
+        (window[_0x4da5e2(0x173)][_0x4da5e2(0x15d)] = 'https://manga.arius.cloud/sussy_baka')
+    }
+    window[_0x2994ca(0x172)]
+      ? document[_0x2994ca(0x16f)] === _0x2994ca(0x16c) || document['readyState'] === 'interactive'
+        ? (_0x3c224c(),
+          window['attachEvent'](_0x2994ca(0x160), _0x3c224c),
+          window[_0x2994ca(0x172)](_0x2994ca(0x16b), _0x3c224c),
+          window[_0x2994ca(0x172)](_0x2994ca(0x161), _0x3c224c),
+          window[_0x2994ca(0x172)](_0x2994ca(0x162), _0x3c224c))
+        : setTimeout(argument['callee'], 0x0)
+      : (window[_0x2994ca(0x15f)](_0x2994ca(0x169), _0x3c224c),
+        window['addEventListener']('resize', _0x3c224c),
+        window[_0x2994ca(0x15f)]('mousemove', _0x3c224c),
+        window[_0x2994ca(0x15f)]('focus', _0x3c224c),
+        window[_0x2994ca(0x15f)](_0x2994ca(0x164), _0x3c224c))
+  })()
+function _0x42d8() {
+  var _0x689444 = [
+    '16261310dUNScT',
+    'addEventListener',
+    'onresize',
+    'onfocus',
+    'onblur',
+    '10011717kSWzBP',
+    'blur',
+    '7kXGJKU',
+    '6611uTNCxP',
+    '328368FphGZv',
+    '1014164AkERLx',
+    'load',
+    '242030UTLEeA',
+    'onmousemove',
+    'complete',
+    '10AkUDvm',
+    '3719488ysazPu',
+    'readyState',
+    '6DCFIQf',
+    '40rlOPhK',
+    'attachEvent',
+    'location',
+    '10CtxKgn',
+    '12648Cdbhos',
+    'href',
+  ]
+  _0x42d8 = function () {
+    return _0x689444
+  }
+  return _0x42d8()
+}
