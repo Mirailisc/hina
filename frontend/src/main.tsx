d3fc344bb50ca868e7b8a0440d2d21248040c46b
Mirailisc <mirailisclm@gmail.com>
2025-01-10 00:46:34 +0700

    add google analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import { BrowserRouter } from 'react-router-dom'
 
 import * as Sentry from '@sentry/react'
@@ -6,17 +7,22 @@ import * as Sentry from '@sentry/react'
 import App from './App.tsx'
 import './index.css'
 
-Sentry.init({
-  dsn: 'https://30d77b5a17db730c06d9c37a39b9b6af@o4508484759453696.ingest.us.sentry.io/4508501816770560',
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-})
+const isProd = import.meta.env.MODE === 'production'
+
+if (isProd) {
+  Sentry.init({
+    dsn: 'https://30d77b5a17db730c06d9c37a39b9b6af@o4508484759453696.ingest.us.sentry.io/4508501816770560',
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  })
+}
+
+if (isProd) {
+  ReactGA.initialize('G-6VHE5KP5H2')
+}
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
